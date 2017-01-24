0f0d81c8f2790a164333fa334bc4755298b44a13
Marcel Goethals <marcel.goethals@gmail.com>
2017-01-24 14:44:51 +0100

    menubar

--- src/js/MenuBar.js
+++ src/js/MenuBar.js
@@ -1,25 +1,39 @@
 var MenuBar = (function(){
 
-	var style = s.cl({
-		//shape
-		"box-sizing": "border-box",
-		"height": "70px",
-		"width": "100%",
-		"top": "0px",
-		"line-height": "70px",
-		"padding-left": "20px",
-		"padding-right": "20px",
+	var style = {
+		parent: s.cl({
+			//shape
+			"box-sizing": "border-box",
+			"height": "70px",
+			"width": "100%",
+			"top": "0px",
+			"line-height": "70px",
+			"padding-left": "20px",
+			"padding-right": "0px",
 
-		//styling
-		"background-color": "#1a555c",
-		"color": "#fff",
-		"box-shadow": "0px 1px 5px #AAAAAA",
-	});
+			//styling
+			"background-color": "#1a555c",
+			"color": "#fff",
+			"box-shadow": "0px 1px 5px #AAAAAA",
+		}),
+		menuitem: s.cl({
+			"float": "right",
+			"padding-left": "20px",
+			"padding-right": "20px",
+			"cursor": "pointer",
+			"line-height": "66px",
+			"border-bottom": "4px solid white"
+		})
+	};
+
 
 	return {
 		view: function(){
-			return m("nav", {class: style}, [
-				m("span","Afvalstoffen tool")
+			return m("nav", {class: style.parent}, [
+				m("span","Afvalstoffen tool"),
+				m("span", {class: style.menuitem},"Overzicht"),
+				m("span", {class: style.menuitem},"Mijn Kosten"),
+
 			]);
 		}
 	};
